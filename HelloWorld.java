2a44422304d7f5f5941d53d01d6d48d300dd2d22
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-02-24 22:00:14 +0530

    every minute update

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 public class HelloWorld{
     public static void main(String args[]){
-        System.out.println("Hello Jenkins I am Here at You");
+        System.out.println("Hello Jenkins I am Here at You every minute");
     }
 }
