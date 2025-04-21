55b61acf25af8288774167065d02b4042cce7ce5
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-04-21 20:33:08 +0530

    Updated External

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("Hello Jenkins I am Here at You every minute");
+        System.out.println("Today is external examination");
     }
 }
